5d9ced5f1cab0b9e927e86eb93071c72ebd44017
jhall0310 <jonathanphall@me.com>
2017-01-20 06:45:42 -0800

    initial push to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
 
   has_many :inspections, dependent: :destroy
   has_many :jobs
+
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
 end
