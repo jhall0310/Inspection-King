6aebc2012658bbb16ca318d416d5405508898b90
jhall0310 <jonathanphall@me.com>
2017-01-25 12:06:01 -0800

    added error handling to user and styling to mailers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
    if user && user.authenticate(params[:password])
      # Save the user id inside the browser cookie. This is how we keep the user
      # logged in when they navigate around our website.
+     flash[:notice] = "You have succcessfully logged in."
      session[:user_id] = user.id
      redirect_to '/'
    else
@@ -18,6 +19,7 @@ class SessionsController < ApplicationController
 
  def destroy
    session[:user_id] = nil
+   flash[:notice] = "You have succcessfully logged out."
    redirect_to '/'
  end
 end
